d1550aa8a5c2148b64f40af524bac566be524109
= <sravangaming7310@gmail.com>
2024-04-25 23:07:45 +0530

    get planets

--- client/src/hooks/requests.js
+++ client/src/hooks/requests.js
@@ -1,6 +1,11 @@
+const API_URL = "http://localhost:8000";
+
 async function httpGetPlanets() {
   // TODO: Once API is ready.
   // Load planets and return as JSON.
+  const data = await fetch(`${API_URL}/planets`);
+  const json = await data.json();
+  return json;
 }
 
 async function httpGetLaunches() {
@@ -18,9 +23,4 @@ async function httpAbortLaunch(id) {
   // Delete launch with given ID.
 }
 
-export {
-  httpGetPlanets,
-  httpGetLaunches,
-  httpSubmitLaunch,
-  httpAbortLaunch,
-};
+export { httpGetPlanets, httpGetLaunches, httpSubmitLaunch, httpAbortLaunch };
