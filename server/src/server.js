5f7618915959402f520def10a88b8e0162e6a7ad
= <sravangaming7310@gmail.com>
2024-05-04 16:50:58 +0530

    add mongoose to server and connect to mongodb atlas

--- server/src/server.js
+++ server/src/server.js
@@ -1,4 +1,5 @@
 const http = require("http");
+const mongoose = require("mongoose");
 
 const app = require("./app");
 const { loadPlanetsData } = require("./models/planets.models");
@@ -7,7 +8,19 @@ const PORT = process.env.PORT || 8000;
 
 const server = http.createServer(app);
 
+const MONGO_URL =
+  "mongodb+srv://nasa-admin:brv603pqrRtvMNxa@nasacluster.b8jj8oq.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster";
+
+mongoose.connection.once("open", () => {
+  console.log("MongoDB connection ready");
+});
+
+mongoose.connection.on("error", (err) => {
+  console.error(err);
+});
+
 async function startServer() {
+  await mongoose.connect(MONGO_URL);
   await loadPlanetsData();
 
   server.listen(PORT, () => {
