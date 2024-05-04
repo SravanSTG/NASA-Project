ede949c56a6a26d566bdd79d2ab399128ac3e17c
= <sravangaming7310@gmail.com>
2024-05-04 22:56:08 +0530

    create schema and model for planets and launches

---
+++ server/src/models/launches.mongo.js
@@ -0,0 +1,39 @@
+const mongoose = require("mongoose");
+
+const launchesSchema = new mongoose.Schema({
+  flightNumber: {
+    type: Number,
+    required: true,
+  },
+  mission: {
+    type: String,
+    required: true,
+  },
+  rocket: {
+    type: String,
+    required: true,
+  },
+  launchDate: {
+    type: Date,
+    required: true,
+  },
+  target: {
+    type: String,
+    required: true,
+  },
+  customers: [String],
+  upcoming: {
+    type: Boolean,
+    required: true,
+  },
+  success: {
+    type: Boolean,
+    required: true,
+    default: true,
+  },
+});
+
+// The model name or the first argument must always be a singular name
+// MongoDB will take the singular name, lower case it, make it plural and assign it as the collection name
+// In this case MongoDB will create a collection called 'launches' and connect it to launchesSchema
+module.exports = mongoose.model("Launch", launchesSchema);
