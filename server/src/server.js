26a289f1ccd018a6890ee0edda53df9ab861dc7f
= <sravangaming7310@gmail.com>
2024-05-05 16:26:14 +0530

    add planets to mongo collection

--- server/src/server.js
+++ server/src/server.js
@@ -9,7 +9,7 @@ const PORT = process.env.PORT || 8000;
 const server = http.createServer(app);
 
 const MONGO_URL =
-  "mongodb+srv://nasa-admin:brv603pqrRtvMNxa@nasacluster.b8jj8oq.mongodb.net/?retryWrites=true&w=majority&appName=NASACluster";
+  "mongodb+srv://nasa-admin:brv603pqrRtvMNxa@nasacluster.b8jj8oq.mongodb.net/nasa?retryWrites=true&w=majority&appName=NASACluster";
 
 mongoose.connection.once("open", () => {
   console.log("MongoDB connection ready");
